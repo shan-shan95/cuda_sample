ea1188b5bbdd966eb40a26ae36835716fec2ebca
shang-shang95 <qqxn7ra9k@gmail.com>
2018-10-15 17:37:22 +0900

    Increment data size

--- sharedSample.cu
+++ sharedSample.cu
@@ -32,9 +32,9 @@ int main(int argc, char **argv) {
   printf("%s Starting...\n", argv[0]);
 
   //行列のデータサイズを指定
-  int nx = 1 << 9;
-  int ny = 1 << 9;
-  int nz = 1 << 9;
+  int nx = 1 << 10;
+  int ny = 1 << 10;
+  int nz = 1 << 10;
 
   int nxyz = nx * ny * nz;
   int nBytes = nxyz * sizeof(float);
