658e3cfbc50afc4359d68dbb1161967becd53288
shang-shang95 <qqxn7ra9k@gmail.com>
2018-10-30 16:21:03 +0900

    Update resister version

--- resisterSample.cu
+++ resisterSample.cu
@@ -4,6 +4,8 @@
 
 __global__ void culCellResister(int nx, int ny, int nz) {
   int cut_num;
+  int cut_test;
+  double t;
 
   //実行時間155usほど。つまりシェアドメモリそのままと変わらない
   //カーネル内で変数宣言すればレジスタに乗ると思っていたが、間違えていた
