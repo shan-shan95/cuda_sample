aff58ce36381ea617ce5974ccd8f8e4ed2f7dc19
shang-shang95 <qqxn7ra9k@gmail.com>
2018-11-11 13:51:08 +0900

    Complete

--- print_sph_efficient_3dcircle.c
+++ print_sph_efficient_3dcircle.c
@@ -1,32 +1,108 @@
 #include <stdio.h>
+#include <math.h>
 #define SIZE 13
+#define RANGE 4
 
-int main(void) {
-	typedef struct {
-		double x;
-		double y;
-		double z;
-	} point;
-	point p[SIZE][SIZE][SIZE];
-	int i, j, k;
+int p2[SIZE][SIZE][SIZE];
 
-	for(i=0; i<SIZE; i++) {
-		for(j=0; j<SIZE; j++) {
-			for(k=0; k<SIZE; k++) {
-				p[k][j][i].x = k;
-				p[k][j][i].y = j;
-				p[k][j][i].z = i;
-			}
-		}
-	}
+void range(double a, double b, double c)
+{
+  typedef struct
+  {
+    double x;
+    double y;
+    double z;
+    int flag;
+  } fun_point;
+  int i, j, k;
+  fun_point fp[SIZE][SIZE][SIZE];
 
-	for (i = 0; i < SIZE; i++) {
-		for (j = 0; j < SIZE; j++) {
-			for (k = 0; k < SIZE; k++) {
-				// 注目セルとの距離計算
-				printf("(%2d, %2d, %2d) ", (int)p[k][j][i].x, (int)p[k][j][i].y, (int)p[k][j][i].z);
-			}
-			printf("\n");
-		}
-	}
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      for (k = 0; k < SIZE; k++)
+      {
+        fp[k][j][i].x = k;
+        fp[k][j][i].y = j;
+        fp[k][j][i].z = i;
+        fp[k][j][i].flag = 0;
+      }
+    }
+  }
+
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      for (k = 0; k < SIZE; k++)
+      {
+        // 注目セルとの距離計算
+        if (pow(a - fp[k][j][i].x, 2.0) + pow(b - fp[k][j][i].y, 2.0) + pow(c - fp[k][j][i].z, 2.0) <= RANGE * RANGE)
+        {
+          fp[k][j][i].flag = 1;
+        }
+      }
+    }
+  }
+
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      for (k = 0; k < SIZE; k++)
+      {
+        p2[k][j][i] *= fp[k][j][i].flag;
+      }
+    }
+  }
+}
+
+int main(void)
+{
+  typedef struct
+  {
+    double x;
+    double y;
+    double z;
+    int flag;
+  } point;
+
+  int i, j, k;
+
+  for (i = 0; i < SIZE; i++)
+  {
+    for (j = 0; j < SIZE; j++)
+    {
+      for (k = 0; k < SIZE; k++)
+      {
+        p2[k][j][i] = 1;
+      }
+    }
+  }
+
+  range(4, 5, 5);
+  range(4, 6, 5);
+  range(4, 5, 6);
+  range(4, 6, 6);
+  range(8, 5, 5);
+  range(8, 6, 5);
+  range(8, 5, 6);
+  range(8, 6, 6);
+
+  for (i = 0; i < SIZE; i++)
+  {
+    printf("z=%d\n", i);
+    for (j = 0; j < SIZE; j++)
+    {
+      for (k = 0; k < SIZE; k++)
+      {
+        printf("%d ", p2[k][j][i]);
+      }
+      printf("\n");
+    }
+    printf("\n\n\n\n");
+  }
+
+  return 0;
 }
